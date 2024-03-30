7702e93e0c5e69cc47729ae16c458b23b60672bd
vikramsingh863 <vikramsinghrathore863@gmail.com>
2024-03-30 16:32:53 +0530

    5

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
+        <footer className="bg-black border-y">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
+                                src="coding.gif"
+                                className="codingimg"
                                 alt="Logo"
                             />
                         </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
+// import './App.css'
 import './index.css'
 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
