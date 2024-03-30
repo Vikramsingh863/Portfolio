1ae1f31c5bc2b4b724ebdaf509571691ebb248d0
vikramsingh863 <vikramsinghrathore863@gmail.com>
2024-03-30 17:08:32 +0530

    6

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,10 +3,10 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="sticky z-50 top-0">
-            <ul class="nav nav-tabs  borders">
-                
-                
+
+        <div className="nav">
+            
+            <ul className="navul">
                 <li class="nav-item px-3">
                     <NavLink
                         to="./"
@@ -17,41 +17,46 @@ export default function Header() {
                         Home
                     </NavLink>
                 </li >
-                <NavLink
-                            to="about"
-                            className={({ isActive }) =>
-                                `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                            }
-                        >
-                            About
-                        </NavLink>
-
 
                 <li class="nav-item px-3">
-                   
+                    <NavLink
+                        to="about"
+                        className={({ isActive }) =>
+                            `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                        }
+                    >
+                        About
+                    </NavLink>
                 </li>
+
+
                 <li class="nav-item  px-3">
-                <NavLink
-                            to="Contact"
-                            className={({ isActive }) =>
-                                `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                            }
-                        >
-                            Contact
-                        </NavLink>
+                    <NavLink
+                        to="Contact"
+                        className={({ isActive }) =>
+                            `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                        }
+                    >
+                        Contact
+                    </NavLink>
                 </li>
+
                 <li class="nav-item px-3">
-                <NavLink
-                            to="https://github.com/Vikramsingh863"
-                            className={({ isActive }) =>
-                                `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                            }
-                        >
-                            GitHub
-                        </NavLink>
+                    <NavLink
+                        to="https://github.com/Vikramsingh863"
+                        className={({ isActive }) =>
+                            `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"}lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                        }
+                    >
+                        GitHub
+                    </NavLink>
                 </li>
             </ul>
+            <img src="portfolio-white.png" className="navimg" alt="error" />
+
         </div>
+
+
     );
 }
 
